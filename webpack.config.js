73430df6cd0b321877dbecda58e80f3a9c4819ba
bubucong <liusc86@sina.com>
2016-07-26 17:07:18 +0800

    newest config file

--- webpack.config.js
+++ webpack.config.js
@@ -7,29 +7,39 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
     //webpack打包入口点
     entry:{
-        demo1:__dirname+"/src/libs/Demo1.js",
+        //__dirname变量中保存了当前项目的根目录
+        'Demo/demo1':__dirname+"/src/Demo/demo1.js",
+        'MainPage/MainPage':__dirname+"/src/mainPage/MainPage.js",
         vendor: ['jquery'] //第三方库
     },
     //webpack打包输出点
     output:{
-        path:__dirname+"/dist/libs",
-        filename:"[name]_bundle.js"
+        path:__dirname+"/dist/",
+        filename:"[name]_bundle.js",//[chunkhash:6]这里的意思是动态添加6位hash码，从而使浏览器的js文件保持更新而不会被缓存
+        chunkFilename: "[name].chunk.js",
+        publicPath:"/GulpWebpack/dist/"
     },
     module:{
         loaders:[
             {test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
             {test: /\.jsx?$/,exclude: /node_modules/,loaders: ['babel']},
-            {test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader")},
-            {test: /\.scss$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader") }
+            {test: /\.css$/,loader:'style!css'},
+            {test: /\.scss$/,loader: 'style!css!sass'}
+            //{test: /\.css$/,loader:ExtractTextPlugin.extract('style-loader','css-loader')},
+            //{test: /\.scss$/,loader: ExtractTextPlugin.extract('style-loader','css-loader!sass-loader')}
         ]
     },
     resolve:{
-        root: [process.cwd() + '/src', process.cwd() + '/node_modules'],
+        root: [process.cwd() + '\\src', process.cwd() + '\\node_modules'],
         extensions: ['', '.js', '.json', '.scss']
     },
     plugins: [
-        new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor_bundle.js'),//这是第三方库打包生成的文件
-        new ExtractTextPlugin("[name].css")//在这里将样式文件抽离成单独的css文件
+        new webpack.optimize.CommonsChunkPlugin('vendor','common/libs/vendor_bundle.js'),//这是第三方库打包生成的文件
+        new ExtractTextPlugin("[name].css"),//在使用这个插件的时候，会对所有的入口点文件生成独立的css,但是在所有的入口点文件中所引用的样式文件，都会被合并到生成的入口点样式文件当中。
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery"
+        })
     ]
-
 }
